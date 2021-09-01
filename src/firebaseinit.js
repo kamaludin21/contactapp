f86a7dca5010a6c9b488c9d961444a40bd0bd797
Kamaludin <kamal.zyel@gmail.com>
2021-09-01 14:34:07 +0700

    initial firebase + config

---
+++ src/firebaseinit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_PROJECT_ID+".firebaseapp.com",
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_PROJECT_ID+".appspot.com",
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
